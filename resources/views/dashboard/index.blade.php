c38ca32c755cdef6dc79f1fdc938b51ed1d44390
owen <owen.djohan@gmail.com>
2024-10-30 16:57:48 +0700

    perbaikan logic untuk weekly spendable

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -356,7 +356,7 @@
                                                                                                         Week)
                                                                                                 </div>
                                                                                         @php
-                                                                                $anggaran = ($spendable / 48) - $pengeluaran_mingguan
+                                                                                $anggaran = (($spendable + $pengeluaran_mingguan)/48) - $pengeluaran_mingguan
                                                                                         @endphp
                                                                                                 <div
                                                                                                         class="h5 mb-0 font-weight-bold text-gray-800">
