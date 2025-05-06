39f382933e3af7aaab14937dda80b05ab2b97031
Genex1s <owen.djohan@gmail.com>
2025-05-06 17:31:09 +0700

    Add import data button and update dashboard title with year

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -303,6 +303,14 @@
                             class="fas fa-download fa-sm text-white-50"></i>
                             Backup Data</a>
                         </div>
+
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                            <h1 class="h3 mb-0 text-gray-800">Dashboard | {{ $tahun }}</h1>
+                            <a href="/importAll"
+                            class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                            class="fas fa-download fa-sm text-white-50"></i>
+                            Import Data</a>
+                        </div>
                         @endcan
 
                         <!-- Content Row -->
